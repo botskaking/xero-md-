ad43dc882598ba3bd296fb090f5968905baca7a4
botskaking <152233761+botskaking@users.noreply.github.com>
2024-02-24 09:45:46 +0530

    Update BotCharacters.js

--- System/BotCharacters.js
+++ System/BotCharacters.js
@@ -1,34 +1,3 @@
-/* ---------------------------------------------------------------------------------/
-/                                                                                   /
-/             d8888 888    888                        888b     d888 8888888b.       /
-/            d88888 888    888                        8888b   d8888 888  "Y88b      /
-/           d88P888 888    888                        88888b.d88888 888    888      /
-/          d88P 888 888888 888  8888b.  .d8888b       888Y88888P888 888    888      /
-/         d88P  888 888    888     "88b 88K           888 Y888P 888 888    888      /
-/        d88P   888 888    888 .d888888 "Y8888b.      888  Y8P  888 888    888      /
-/       d8888888888 Y88b.  888 888  888      X88      888   "   888 888  .d88P      /
-/      d88P     888  "Y888 888 "Y888888  88888P'      888       888 8888888P"       /
-/                                                                                   / 
-/-----------------------------------------------------------------------------------/
-/ Author and Main Developer: FantoX                                                 /
-/ Github: https://github.com/FantoX001/Atlas-MD                                     /
-/ Powered By: Team ATLAS                                                            /
-/-----------------------------------------------------------------------------------/
-/             Meet Team ATLAS who holds all rights to this repository:              /
-/                                                                                   /
-/ 1. Pratyush - https://github.com/pratyush4932                                     /
-/ 2. Ahmii - https://github.com/Ahmii-kun                                           /               
-/ 3. Kai - https://github.com/Kai0071                                               /                    
-/ 4. Devime - https://github.com/Devime69                                           /
-/ 5. Jay JayOps - https://github.com/jayjay-ops                                     /
-/                                                                                   /
-/ ----------------------------------------------------------------------------------/
-/                                                                                   /
-/      With all of our hard work and defication you can enjoy this awesome bot!     /  
-/                                                                                   / 
-/----------------------------------------------------------------------------------*/
-
-
 
 /* ---Added Anime Character list---  /
  0 --- Atlas MD
@@ -50,7 +19,7 @@
 16 --- Mai
 17 --- Yor
 18 --- Shinbou
-19 --- Eiko
+19 --- Xero
 
 Image Source: https://wall.alphacoders.com/
 Image Source: https://wallpapercave.com/
@@ -280,12 +249,12 @@ global.charID18 = {
 }
 
 global.charID19 = {
-  botName : "Eiko",
-  botVideo : "https://graph.org/file/7f99357fec0f000c7ab0a.mp4",
-  botImage1 : "https://images8.alphacoders.com/122/1229829.jpg",
-  botImage2 : "https://images2.alphacoders.com/123/1234944.png",
-  botImage3 : "https://images5.alphacoders.com/123/1235274.jpg",
-  botImage4 : "https://images6.alphacoders.com/126/1260043.png",
-  botImage5 : "https://images6.alphacoders.com/124/1247977.jpg",
-  botImage6 : "https://images2.alphacoders.com/125/1251795.jpg",
+  botName : "Xero",
+  botVideo : "https://mobstatus.com/wp-content/uploads/2023/01/Alan-Walker-Alone-Pt.-II-Lyrics-Status-_-Whatsapp-Status-_-_Your-Name-Edit_-_-Canorous-u720P_HD.mp4",
+  botImage1 : "https://i.imgur.com/BX4wBp8.jpeg",
+  botImage2 : "https://i.imgur.com/BX4wBp8.jpeg",
+  botImage3 : "https://i.imgur.com/BX4wBp8.jpeg",
+  botImage4 : "https://i.imgur.com/BX4wBp8.jpeg",
+  botImage5 : "https://i.imgur.com/BX4wBp8.jpeg",
+  botImage6 : "https://i.imgur.com/BX4wBp8.jpeg",
 }
